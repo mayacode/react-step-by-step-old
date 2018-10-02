550d83fb752ca8a190a02eabe403cf1c9a2b320e
Maja Miarecki <mmiarecka@gmail.com>
2018-10-02 19:23:47 +0200

    Lesson 2 - simple react app

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./app/index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    }
+};
