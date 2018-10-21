d83490313963b8bda8e24c2bbef3a88e741abfd1
Maja Miarecki <mmiarecka@gmail.com>
2018-10-21 10:54:37 +0200

    made 100% coverage in tested files

--- app/components/BookList.test.js
+++ app/components/BookList.test.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import BookList from './BookList';
+import BookListItem from './BookListItem';
+
+const bookList = [
+  {
+    id: '1234qwer',
+    volumeInfo: {
+      title: 'A Book'
+    }
+  },
+  {
+    id: '1235qwer',
+    volumeInfo: {
+      title: 'A Book 2'
+    }
+  }
+];
 
 describe('<BookList />', () => {
-  it('should match snapshot', () => {
-    const wrapper = shallow(<BookList books={[]} />);
+  const wrapper = shallow(<BookList books={bookList} />);
+
+  it('matches a snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has 2 BookListItem elements', () => {
+    const items = wrapper.find(BookListItem);
+    expect(items.length).toEqual(2);
   });
 });
